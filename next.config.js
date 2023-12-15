5815a240ab447aa597dbe49f2266be58869bb1c5
Evi <69012052+Evi-2003@users.noreply.github.com>
2023-12-15 22:10:53 +0100

    Sitemap

--- next.config.js
+++ next.config.js
@@ -1,22 +1,6 @@
 /** @type {import('next').NextConfig} */
-const csvLoader = {
-  test: /\.csv$/,
-  use: [
-    {
-      loader: 'csv-loader',
-      options: {
-        dynamicTyping: true,
-        header: true,
-        skipEmptyLines: true
-      }
-    }
-  ]
-};
+
 module.exports = {
-  webpack: (config) => {
-    config.module.rules.push(csvLoader);
-    return config;
-  },
   images: {
     remotePatterns: [
       {
