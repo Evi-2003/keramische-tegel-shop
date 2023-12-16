a052173afd6dd703327a8ff713c452b4b9cab541
Evi <69012052+Evi-2003@users.noreply.github.com>
2023-12-16 14:58:58 +0100

    Darkmode & Tekst

--- app/success/page.js
+++ app/success/page.js
@@ -11,9 +11,10 @@ myConfetti({
   spread: 320
 });
   return (
-    <main className="p-5 text-5xl font-semibold text-center h-[80vh] flex flex-col items-center justify-center">
-      <h1>Yes! Je bestelling is geplaatst!</h1>
+    <main className="p-5 text-5xl font-semibold text-center h-[60vh] dark:text-slate-100 flex flex-col items-center justify-center gap-y-2">
+      <h1>Je bestelling is geplaatst!</h1>
       <h2 className="text-2xl">Bedankt voor je bestelling. Je ontvangt straks een bevestiging.</h2>
+      <span className='text-xl'>Vragen over de bestelling? Neem contact op via <a href='tel:0317 765 005' className='hover:scale-95 underline underline-offset-4'>0317 765 005</a></span>
       
     </main>
   )
